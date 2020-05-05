c17ff46ac8bf71bf37eccb51a16c4f017f60b997
geb <george.beers13@gmail.com>
2020-05-05 22:54:42 +0100

    make the cursor the height of the font, not the bar

--- dmenu.c
+++ dmenu.c
@@ -149,7 +149,7 @@ drawmenu(void)
 		curpos = TEXTW(text) - TEXTW(&text[cursor]);
 		if ((curpos += lrpad / 2 - 1) < w) {
 			drw_setscheme(drw, scheme[SchemeNorm]);
-			drw_rect(drw, x + curpos, 2, 2, bh - 4, 1, 0);
+			drw_rect(drw, x + curpos + 1, (bh - drw->fonts->h) / 2, 1, drw->fonts->h, 1, 0);
 		}
 	}
 
